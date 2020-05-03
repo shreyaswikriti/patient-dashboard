bf8a3d20b516a3070d0c50148b37e4b66480eb0a
shreyaswikriti <57635473+shreyaswikriti@users.noreply.github.com>
2020-05-03 19:08:36 +0530

    patient profile added

--- project_dashboard/patient/views.py
+++ project_dashboard/patient/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from login_reg.decorators import allowed_roles
 
+
+
+from .models import PatientProfile,PatientAddress,PatientTreatment
+
 # Create your views here.
 import logging
 
@@ -12,5 +16,16 @@ def patient_detail(request):
 	# logger.info("Requesting for registration or Login Page")
 	return render(request, 'patient_dashboard.html', {})
 
+
 def patient_profile(request):
-	return render(request, 'patient_profile.html', {})
+	try:
+		detail=PatientProfile.objects.filter(user=request.user).first()
+		logger.info("Name:{} {} dob: {} and Gender: {}".format(detail.firstName, detail.lastName, detail.dob, detail.gender))
+		details=PatientAddress.objects.filter(user=request.user).first()
+		logger.info("Address:{} city:{} District: {} Locality: {} State: {} Pincode: {} Nationality: {} contactno:{}".format(details.Address, details.city, details.district, details.locality,details.state,details.pincode,details.nationality,details.contactno))
+		T=PatientTreatment.objects.filter(patient__user=request.user).first()
+		logger.info("Diagnosis:{} Prescription: {} ".format(T.diagnosis, T.prescription))
+	except:
+		logger.error("oject not found")
+	return render(request, 'patient_profile.html', {"detail":detail,"details":details,"T":T}) 
+	
